b84148e3bac94e39362f9126bbd2a0664936a4da
leen1515 <leventredeparis@gmail.com>
2023-03-06 19:04:41 +0100

    feat (index) : url des vignettes active

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,21 +1,66 @@
 function photographerFactory(data) {
-    const { name, tagline, city, country, price, portrait } = data;
+    const { name, id, tagline, city, country, price, portrait } = data;
 
-    const picture = `./assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${portrait}`;
+
+
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const urlPage = document.createElement('a');
+        urlPage.setAttribute("href", `./photographer.html/${id}`);
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
+        urlPage.appendChild(img);
+        article.appendChild(urlPage);
         article.appendChild(h2);
         const text = document.createElement('p');
-        text.setAttribute("class", "textDescription");
+        text.setAttribute("class", "texte-description");
         text.innerHTML = `${tagline}<br> localisation : ${city}, ${country} <br> tarif : ${price}€/heure`;
         article.appendChild(text);
         return (article);
     }
-    return { name, picture, tagline, city, country, price, getUserCardDOM }
+
+
+    return { name, id, picture, tagline, city, country, price, getUserCardDOM };
 }
+
+
+
+function photoFactory(data, photographerName) {
+    const { photographerId, title, image, likes, date, price } = data;
+    const { name } = photographerName ;
+
+    function garderPrenom (name){
+    var i;
+    var tmp="";
+    for( i = name.length -1; i > -1; i--){
+    if( name.charAt(i) == " ")
+    break;
+    }
+    tmp = name.substring(0, i);
+    return( tmp);
+    } 
+
+
+    const picturePhoto = `./assets/images/${garderPrenom(name)}/${image}`;
+
+    function getPhotoCardDOM() {
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute("src", picturePhoto)
+        const h2 = document.createElement('h2');
+        h2.textContent = title;
+        article.appendChild(img);
+        article.appendChild(h2);
+
+        return (article);
+    }
+
+    return { photographerId, title, image, likes, date, price, getPhotoCardDOM }
+}
+
+
+
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,46 @@
+<<<<<<< Updated upstream
 //Mettre le code JavaScript lié à la page photographer.html
+=======
+//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+ 
+
+    let photographersJson = await fetch("./data/photographers.json"); //lecture du lien json
+    let photographers = await photographersJson.json(); //promet un objet json
+
+    return photographers;
+}
+
+//ajout de l'argument media pour recuperer les photos respectives de chaque photographe
+async function displayData(photographers, media) {
+    const photosSection = document.querySelector(".photos_section");
+
+    media.forEach((photo) => {
+        const photographerName = photographers.find(photographer => photographer.id === photo.photographerId);
+        const photoModel = photoFactory(photo, photographerName);
+        const photoCardDOM = photoModel.getPhotoCardDOM();
+        photosSection.appendChild(photoCardDOM);
+
+     
+
+    });
 
 
+
+}
+
+
+var pathname = window.location.href;
+console.log(pathname);
+
+
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers, media } = await getPhotographers(); //recupere en plus la partie media
+
+    displayData(photographers, media); //retourne la partie photographers et media du tableau json
+}
+
+init();
+>>>>>>> Stashed changes
