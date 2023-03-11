9814e62d61a9f4dd3bd8b8c8f2d268c43474ae76
leen1515 <leventredeparis@gmail.com>
2023-03-11 09:04:06 +0100

    refactor:fonction

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -38,35 +38,26 @@ function photographerFactory(data) {
     return { name, id, picture, tagline, city, country, price, getUserCardDOM };
 }
 
+function garderPrenom(name) {
+    let i;
+    let tmp = "";
+    for (i = name.length - 1; i > -1; i--) {
+        if (name.charAt(i) == " ")
+            break;
+    }
+    tmp = name.substring(0, i);
+    return (tmp);
+}
 
 
 function photoFactory(data, photographerName) {
     const { photographerId, title, image, likes, date, price } = data;
-    const { name } = photographerName;
+    const name = garderPrenom(photographerName);
 
-    function garderPrenom(name) {
-        let i;
-        let tmp = "";
-        for (i = name.length - 1; i > -1; i--) {
-            if (name.charAt(i) == " ")
-                break;
-        }
-        tmp = name.substring(0, i);
-        return (tmp);
-    }
-
-
-
-    const picturePhoto = `assets/images/${garderPrenom(name)}/${image}`;
-
-
-
-
+   
+    const picturePhoto = `assets/images/${name}/${image}`;
 
     function getPhotoCardDOM() {
-
-
-
 
         const figure = document.createElement("figure");
         const img = document.createElement("img");
